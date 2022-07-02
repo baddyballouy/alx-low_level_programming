64b65b280f74939daa62fbd8357e2513ea67b881
Hammond Jeriel <rieljay9@gmail.com>
2022-07-01 17:21:57 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Hello, puts C Program'
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
